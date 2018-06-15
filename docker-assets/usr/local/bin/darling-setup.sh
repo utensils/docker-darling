cd2aa41279ffb99364571944c13fcda30e7c718b
James Brink <brink.james@gmail.com>
2018-06-15 05:21:47 -0700

    Updated build, added kernel-headers script

--- docker-assets/usr/local/bin/darling-setup.sh
+++ docker-assets/usr/local/bin/darling-setup.sh
@@ -2,4 +2,3 @@
 # Copy in osxcross macports
 cp -rv /Volumes/SystemRoot/osxcross/target/macports/pkgs/opt/local/lib /usr/local/lib
 cp -rv /Volumes/SystemRoot/osxcross/target/macports/pkgs/opt/local/libexec /usr/local/libexec
-mkdir -p /Users/darling/Library/Application Support
--- docker-assets/usr/local/bin/entrypoint.sh
+++ docker-assets/usr/local/bin/entrypoint.sh
@@ -2,7 +2,12 @@
 
 # Build kernel module against arch linux kernel
 # This will eventually be handled properly
+
+sudo /usr/local/kernel-headers/arch_linux.sh
+
+mkdir -p /home/darling/build
 cd /home/darling/build
+cmake ..
 sudo make lkm -j"$(nproc)"
 sudo make lkm_install
 sudo xz -d /lib/modules/4.16.13-2-ARCH/extra/darling-mach.ko.xz
@@ -12,7 +17,7 @@ sudo insmod /lib/modules/4.16.13-2-ARCH/extra/darling-mach.ko
 sudo mount -t tmpfs tmpfs /home/darling
 
 # Setup darling env
-darling shell < /usr/local/bin/darling-setup.sh
+sudo darling shell < /usr/local/bin/darling-setup.sh
 
 if [ $# -eq 0 ]; then
 	echo "No command was given to run, exiting."
